273741988e8707d17d18b44fd32d93206f2a801a
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-09-11 23:39:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello, Abhinav 🚀 Your Node.js + Prisma server is working!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: { name, email, password },
+    });
+    res.json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("🚀 Server running at http://localhost:3000");
+});
