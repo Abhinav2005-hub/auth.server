e6b00e7b0dd358f3af86c56aa8005f1d62db3eb7
Abhinav2005-hub <abhinavsaini995@gmail.com>
2025-09-17 00:23:13 +0530

    update controllers

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,7 @@ const express = require("express");
 const authMiddleware = require("../middleware/auth");
 const { getProfile } = require("../controllers/profile");
 const { PrismaClient } = require("@prisma/client");
+const client = require("../config/redis"); // make sure this exists
 
 const prisma = new PrismaClient();
 const profileRouter = express.Router();
@@ -17,23 +18,18 @@ profileRouter.get("/users", async (req, res) => {
     limit = parseInt(limit) || 5;
 
     const skip = (page - 1) * limit;
-
-    // create a unique cache key based on page & limit
     const cacheKey = `users:page=${page}:limit=${limit}`;
 
-    //Check cache
     const cachedData = await client.get(cacheKey);
     if (cachedData) {
       console.log("Serving from Redis cache");
       return res.json(JSON.parse(cachedData));
     }
 
-    
-    //If no cache, fetch from DB
     const users = await prisma.user.findMany({
       skip,
       take: limit,
-      select: { id: true, name: true, email: true }, 
+      select: { id: true, name: true, email: true },
     });
 
     const totalUsers = await prisma.user.count();
@@ -41,10 +37,9 @@ profileRouter.get("/users", async (req, res) => {
 
     const response = { page, limit, totalUsers, totalPages, users };
 
-    //Store result in Redis with 60s expiry
     await client.set(cacheKey, JSON.stringify(response), { EX: 60 });
-
     console.log("Stored in Redis cache");
+
     res.json(response);
   } catch (err) {
     console.error("Error fetching users:", err);
